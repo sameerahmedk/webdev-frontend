f7bc6c97abf3f4a4a6409b522a2e2a4f535e6f1d
Sameer Ahmed Khan <68906588+sameerahmedk@users.noreply.github.com>
2023-04-23 17:22:38 +0500

    Updated index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
-//import styles from '@/styles/Home.module.css';
+import HeroBanner from '@/components/HeroBanner';
+import ProductCard from '@/components/ProductCard';
+import Wrapper from '@/components/Wrapper';
 import { Inter } from 'next/font/google';
 import Head from 'next/head';
 const inter = Inter({ subsets: ['latin'] });
 
-export default function Home() {
+export default function Home({ products }) {
 	return (
 		<>
 			<Head>
@@ -22,11 +24,43 @@ export default function Home() {
 				/>
 			</Head>
 			<main>
-				<h1>Welcome to B2B Marketplace!</h1>
-				{/* <div className=''> */}
-				{/* <LoginForm /> */}
-				{/* <ProductListing /> */}
-				{/* </div> */}
+				<HeroBanner />
+				<Wrapper>
+					{/* heading and paragaph start */}
+					<div className='text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]'>
+						<div className='text-[28px] md:text-[34px] mb-5 font-semibold leading-tight'>
+							Cushioning for Your Miles
+						</div>
+						<div className='text-md md:text-xl'>
+							A lightweight Nike ZoomX midsole is
+							combined with increased stack
+							heights to help provide cushioning
+							during extended stretches of
+							running.
+						</div>
+					</div>
+					{/* heading and paragaph end */}
+
+					{/* products grid start */}
+					<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0'>
+						{products?.data?.map((product) => (
+							<ProductCard
+								key={product?.id}
+								data={product}
+							/>
+						))}
+						{/* <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard /> */}
+					</div>
+					{/* products grid end */}
+				</Wrapper>
 			</main>
 		</>
 	);
