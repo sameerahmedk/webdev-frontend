0e45fa1019b823c8c87bedfd3759138293a3cd74
Sameer Ahmed Khan <68906588+sameerahmedk@users.noreply.github.com>
2023-04-24 23:19:21 +0500

    updated index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import HeroBanner from '@/components/HeroBanner'
-import ProductCard from '@/components/ProductCard'
+import ProductCard, { Product } from '@/components/ProductCard'
 import Wrapper from '@/components/Wrapper'
 import { Inter } from 'next/font/google'
 import Head from 'next/head'
+import products from '@/data/products.json'
+
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home({ products }) {
+export default function Home() {
   return (
     <>
       <Head>
@@ -23,20 +25,18 @@ export default function Home({ products }) {
           {/* heading and paragaph start */}
           <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]">
             <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
-              Cushioning for Your Miles
+              Your one-stop B2B marketplace!
             </div>
             <div className="text-md md:text-xl">
-              A lightweight Nike ZoomX midsole is combined with increased stack
-              heights to help provide cushioning during extended stretches of
-              running.
+              Get all your business needs in one place with the best prices.
             </div>
           </div>
           {/* heading and paragaph end */}
 
           {/* products grid start */}
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
-            {products?.data?.map(product => (
-              <ProductCard key={product?.id} data={product} />
+            {products?.map(product => (
+              <ProductCard key={product?._id} data={product} />
             ))}
             {/* <ProductCard />
                     <ProductCard />
@@ -54,3 +54,11 @@ export default function Home({ products }) {
     </>
   )
 }
+
+// export async function getStaticProps() {
+//   // const products = await fetchDataFromApi('/api/products?populate=*')
+
+//   return {
+//     props: { products }
+//   }
+// }
