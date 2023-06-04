fd755d564860d4374c332610da63e19bdcb13fbb
Sameer Ahmed Khan <68906588+sameerahmedk@users.noreply.github.com>
2023-06-04 21:47:35 +0500

    Implemented cart page

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,20 +1,25 @@
+import {
+  CartItem as typeCartItem,
+  addToCart,
+  removeFromCart
+} from '@/store/cartSlice'
 import Image from 'next/image'
-import React from 'react'
 import { RiDeleteBin6Line } from 'react-icons/ri'
-import { updateCart, removeFromCart } from '@/store/cartSlice'
 import { useDispatch } from 'react-redux'
+import { Product } from '@/types/product'
+
 const CartItem = ({ data }) => {
-  const p = data.attributes
+  const product: Product = data.attributes
 
   const dispatch = useDispatch()
 
   const updateCartItem = (e, key) => {
-    let payload = {
-      key,
+    let payload: typeCartItem = {
+      ...data,
       val: key === 'quantity' ? parseInt(e.target.value) : e.target.value,
       id: data.id
     }
-    dispatch(updateCart(payload))
+    dispatch(addToCart(payload))
   }
 
   return (
@@ -22,8 +27,8 @@ const CartItem = ({ data }) => {
       {/* IMAGE START */}
       <div className="shrink-0 aspect-square w-[50px] md:w-[120px]">
         <Image
-          src={p.thumbnail.data.attributes.url}
-          alt={p.name}
+          src={product.image}
+          alt={product.name}
           width={120}
           height={120}
           style={{
@@ -38,36 +43,37 @@ const CartItem = ({ data }) => {
         <div className="flex flex-col md:flex-row justify-between">
           {/* PRODUCT TITLE */}
           <div className="text-lg md:text-2xl font-semibold text-black/[0.8]">
-            {p.name}
+            {product.name}
           </div>
 
           {/* PRODUCT SUBTITLE */}
-          <div className="text-sm md:text-md font-medium text-black/[0.5] block md:hidden">
-            {p.subtitle}
-          </div>
+          {/*           <div className="text-sm md:text-md font-medium text-black/[0.5] block md:hidden">
+            {product.subtitle}
+          </div> */}
 
           {/* PRODUCT PRICE */}
           <div className="text-sm md:text-md font-bold text-black/[0.5] mt-2">
-            MRP : &#8377;{p.price}
+            MRP : ${product.unitPrice}
           </div>
         </div>
 
         {/* PRODUCT SUBTITLE */}
-        <div className="text-md font-medium text-black/[0.5] hidden md:block">
-          {p.subtitle}
-        </div>
+        {/*         <div className="text-md font-medium text-black/[0.5] hidden md:block">
+          {product.subtitle}
+        </div> */}
 
         <div className="flex items-center justify-between mt-4">
-          <div className="flex items-center gap-2 md:gap-10 text-black/[0.5] text-sm md:text-md">
+          {/* <div className="flex items-center gap-2 md:gap-10 text-black/[0.5] text-sm md:text-md">
             <div className="flex items-center gap-1">
               <div className="font-semibold">Size:</div>
               <select
+                title="Select Size"
                 className="hover:text-black"
                 onChange={e => updateCartItem(e, 'selectedSize')}>
-                {p.size.data.map((item, i) => {
+                {product.size.data.map((item, i) => {
                   return (
                     <option
-                      key={i}
+                      key={item.id}
                       value={item.size}
                       disabled={!item.enabled ? true : false}
                       selected={data.selectedSize === item.size}>
@@ -81,18 +87,19 @@ const CartItem = ({ data }) => {
             <div className="flex items-center gap-1">
               <div className="font-semibold">Quantity:</div>
               <select
+                title="Select Quantity"
                 className="hover:text-black"
                 onChange={e => updateCartItem(e, 'quantity')}>
                 {Array.from({ length: 10 }, (_, i) => i + 1).map((q, i) => {
                   return (
-                    <option key={i} value={q} selected={data.quantity === q}>
+                    <option key={q} value={q} selected={data.quantity === q}>
                       {q}
                     </option>
                   )
                 })}
               </select>
             </div>
-          </div>
+          </div> */}
           <RiDeleteBin6Line
             onClick={() => dispatch(removeFromCart({ id: data.id }))}
             className="cursor-pointer text-black/[0.5] hover:text-black text-[16px] md:text-[20px]"
